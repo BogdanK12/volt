5130443dd42e52f48f1bc7569d56b3172efc1257
BogdanK12 <nadgobshpion002@gmail.com>
2026-06-04 20:38:53 +0300

    Add screan cleaning and fast screen update

--- src/main.zig
+++ src/main.zig
@@ -6,18 +6,23 @@ pub fn main(init: std.process.Init) !void {
     const arena: std.mem.Allocator = init.arena.allocator();
 
     const args = try init.minimal.args.toSlice(arena);
-    _ = &args;
+    _ = args;
     // for (args) |arg| {
     //     std.log.info("arg: {s}", .{arg});
     // }
 
     const io = init.io;
-    const cwd: std.Io.Dir = try std.Io.Dir.cwd().openDir(io, ".", .{ .iterate = true });
+    var cwd: std.Io.Dir = try std.Io.Dir.cwd().openDir(io, ".", .{ .iterate = true });
 
+    const stdout: std.Io.File = .stdout();
+    _ = stdout;
     var stdout_buffer: [1024]u8 = undefined;
     var stdout_file_writer: std.Io.File.Writer = .init(.stdout(), io, &stdout_buffer);
 
-    const arr = try volt.filesys.dirToArray(init.io, init.gpa, cwd);
+    var stdin_buf: [1024]u8 = undefined;
+    var stdin_reader: std.Io.File.Reader = .init(.stdin(), io, &stdin_buf);
+
+    var arr = try volt.filesys.dirToArray(init.io, init.gpa, cwd);
     defer init.gpa.free(arr);
 
     var terminal: Term = undefined;
@@ -25,9 +30,22 @@ pub fn main(init: std.process.Init) !void {
 
     try terminal.enableRawMode();
 
-    for (arr) |file| {
-        try stdout_file_writer.interface.print("{s}\n", .{file});
-        init.gpa.free(file);
+    while (stdin_reader.interface.takeByte()) |byte| {
+        if (byte == 'k') {
+            cwd = try cwd.openDir(io, "..", .{ .iterate = true });
+            for (arr) |file| {
+                init.gpa.free(file);
+            }
+            init.gpa.free(arr);
+            arr = try volt.filesys.dirToArray(io, init.gpa, cwd);
+            try stdout_file_writer.interface.writeAll("\x1b[2J\x1b[H");
+        }
+        for (arr) |file| {
+            try stdout_file_writer.interface.print("{s}\n", .{file});
+        }
+        try stdout_file_writer.interface.flush();
+    } else |err| {
+        return err;
     }
 
     try stdout_file_writer.interface.flush();
